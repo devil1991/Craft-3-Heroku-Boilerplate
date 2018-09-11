540a981fc54d9d8dc0841606a88386d658f220a9
Vadim Goncharov <vadim@roundhouseagency.com>
2018-09-11 12:30:18 -0700

    updating envs

--- config/db.php
+++ config/db.php
@@ -7,15 +7,30 @@
  * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
  */
 
-$url = getenv('JAWSDB_MARIA_URL');
+$environment = getenv('ENVIRONMENT');
 
-$dbparts = parse_url($url);
+if ($environment == 'heroku-dev') {
+    $url = getenv('JAWSDB_MARIA_URL');
 
-return array(
-  'server' => $dbparts['host'],
-  'user' => $dbparts['user'],
-  'password' => $dbparts['pass'],
-  'database' => ltrim($dbparts['path'],'/'),
-  'tablePrefix' => 'craft',
-  'port' => $dbparts['port'],
-);
+    $dbparts = parse_url($url);
+
+    return array(
+        'server' => $dbparts['host'],
+        'user' => $dbparts['user'],
+        'password' => $dbparts['pass'],
+        'database' => ltrim($dbparts['path'],'/'),
+        'tablePrefix' => 'craft',
+        'port' => $dbparts['port'],
+    );
+} else {
+    return [
+        'driver' => getenv('DB_DRIVER'),
+        'server' => getenv('DB_SERVER'),
+        'user' => getenv('DB_USER'),
+        'password' => getenv('DB_PASSWORD'),
+        'database' => getenv('DB_DATABASE'),
+        'schema' => getenv('DB_SCHEMA'),
+        'tablePrefix' => getenv('DB_TABLE_PREFIX'),
+        'port' => getenv('DB_PORT')
+    ];
+}
